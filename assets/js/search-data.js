3200d326e8f045c5656b17121f92283e714d7ee6
iamalinguist <sopan.tripathi@gmail.com>
2025-01-26 18:44:57 +0000

    Deploying to gh-pages from @ iamalinguist/iamalinguist.github.io@1e74c86604e7437ddf3cef8c20f1fdc3a0d26a6e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Download the updated PDF CV to access the latest information. The digital sections here have been optimized with concise details for enhanced readability.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
