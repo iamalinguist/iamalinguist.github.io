88124631fd02d7fab2eda1912b743cc3f89c8a5c
iamalinguist <sopan.tripathi@gmail.com>
2025-01-29 11:37:45 +0000

    Deploying to gh-pages from @ iamalinguist/iamalinguist.github.io@fafe841dcb07c3c0d37a5cf79814903a79470ccd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,7 +423,7 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-begin-syntax-in-hindi",
           title: 'begin syntax in Hindi',
-          description: "academic monograph",
+          description: "academic monograph (VP, NP, Verb Group, Subordination and Coordination)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-word-sense-in-sanskrit",
