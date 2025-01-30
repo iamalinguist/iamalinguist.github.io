f2081e77cbdfc31e4e933dfd376a095be9289424
iamalinguist <sopan.tripathi@gmail.com>
2025-01-30 09:42:49 +0000

    Deploying to gh-pages from @ iamalinguist/iamalinguist.github.io@16b817f933c4403d81ad303094013d73ce6690d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -441,6 +441,11 @@ ninja.data = [{
           description: "a volunteering project to produce effective content on verbs, conversation, script etc.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-a-devnāgarī-and-romanāgrī-writer",
+          title: 'a devnāgarī and romanāgrī writer',
+          description: "a simple app for transliterating Hindi script into Roman and IPA notation.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
